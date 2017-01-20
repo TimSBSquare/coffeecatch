e20c9f8830fe25131edb44381b64619c7fcdfc14
GUO Yixuan <culu.gyx@gmail.com>
2017-01-20 15:31:26 -0500

    sample files to build in android ndk

---
+++ Android.mk
@@ -0,0 +1,23 @@
+LOCAL_PATH := $(call my-dir)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := libcoffeecatch
+LOCAL_SRC_FILES := coffeecatch.c coffeejni.c
+# -Werror disabled
+LOCAL_CFLAGS := -fPIC -O3 -g3 -pthread \
+    		-W -Wall -Wextra -Wno-unused-function \
+		-D_REENTRANT -D_GNU_SOURCE
+LOCAL_LDFLAGS := -shared -fPIC -O3 -Wl,-O1 -Wl,--no-undefined \
+    		-rdynamic -shared
+LOCAL_LDLIBS := -ldl
+include $(BUILD_SHARED_LIBRARY)
+
+include $(CLEAR_VARS)
+LOCAL_MODULE := test
+LOCAL_SHARED_LIBRARIES := libcoffeecatch
+LOCAL_SRC_FILES := sample.c
+LOCAL_CFLAGS := -fPIC -O3 -g3 \
+    		-W -Wall -Wextra -Werror -Wno-unused-function \
+		-D_REENTRANT
+LOCAL_LDFLAGS := -fPIC -O3 -Wl,-O1
+include $(BUILD_EXECUTABLE)
