f03fd7105e016b3addd5a26903e8e94d4b0b406d
GUO Yixuan <culu.gyx@gmail.com>
2017-01-20 15:36:28 -0500

    add CFLAGS

--- Android.mk
+++ Android.mk
@@ -6,7 +6,8 @@ LOCAL_SRC_FILES := coffeecatch.c coffeejni.c
 # -Werror disabled
 LOCAL_CFLAGS := -fPIC -O3 -g3 -pthread \
     		-W -Wall -Wextra -Wno-unused-function \
-		-D_REENTRANT -D_GNU_SOURCE
+		-D_REENTRANT -D_GNU_SOURCE \
+		-funwind-tables -Wl,--no-merge-exidx-entries
 LOCAL_LDFLAGS := -shared -fPIC -O3 -Wl,-O1 -Wl,--no-undefined \
     		-rdynamic -shared
 LOCAL_LDLIBS := -ldl
@@ -18,6 +19,7 @@ LOCAL_SHARED_LIBRARIES := libcoffeecatch
 LOCAL_SRC_FILES := sample.c
 LOCAL_CFLAGS := -fPIC -O3 -g3 \
     		-W -Wall -Wextra -Werror -Wno-unused-function \
-		-D_REENTRANT
+		-D_REENTRANT \
+		-funwind-tables -Wl,--no-merge-exidx-entries
 LOCAL_LDFLAGS := -fPIC -O3 -Wl,-O1
 include $(BUILD_EXECUTABLE)
